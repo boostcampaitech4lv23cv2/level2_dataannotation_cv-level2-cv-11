1c54db0f21c93ca1692c1bb3acb006adc41e3415
jerry-ryu <fbrjs15@naver.com>
2022-12-13 00:05:05 +0000

    fix: inf_viz bug fix

--- code/inference_viz.py
+++ code/inference_viz.py
@@ -20,10 +20,10 @@ def parse_args():
     parser = ArgumentParser()
 
     # Conventional args
-    parser.add_argument('--data_dir', default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/ICDAR17_Korean'))
-    parser.add_argument('--json_dir', default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/ICDAR17_Korean/ufo/train.json'))
-    parser.add_argument('--model_dir', default=os.environ.get('SM_CHANNEL_MODEL', 'trained_models'))
-    parser.add_argument('--output_dir', default=os.environ.get('SM_OUTPUT_DATA_DIR', 'predictions'))
+    parser.add_argument('--data_dir', default='/opt/ml/input/data/ICDAR17_Korean')
+    parser.add_argument('--json_dir', default='/opt/ml/input/data/ICDAR17_Korean/ufo/train.json')
+    parser.add_argument('--model_dir', default= '/opt/ml/code/trained_models')
+    parser.add_argument('--output_dir', default= '/opt/ml/code/predictions')
 
     parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
     parser.add_argument('--input_size', type=int, default=1024)
@@ -37,7 +37,7 @@ def parse_args():
     return args
 
 
-def do_inference(model, ckpt_fpath, data_dir, input_size, batch_size):
+def do_inference(model, ckpt_fpath, data_dir, json_dir ,input_size, batch_size):
     model.load_state_dict(torch.load(ckpt_fpath, map_location='cpu'))
     model.eval()
 
@@ -55,12 +55,12 @@ def do_inference(model, ckpt_fpath, data_dir, input_size, batch_size):
     if len(images):
         by_sample_bboxes.extend(detect(model, images, input_size))
 
-    ufo_result = dict(images=dict())
+    with open(json_dir) as f:  ufo_result = json.load(f)
     pred_sample_bboxes = {}
-    for image, image_fname, bboxes in zip(images, image_fnames, by_sample_bboxes):
-        w, h, _ = image.shape
-        words_info = {idx: dict(points=bbox.tolist()) for idx, bbox in enumerate(bboxes)}
-        ufo_result['images'][image_fname] = dict(words=words_info, img_w=w, img_h=h, tags=None, license_tag=None)
+    for image_fname, bboxes in zip(image_fnames, by_sample_bboxes):
+        words_info = {idx: dict(points=bbox.tolist(), transcription="", language = "",
+                                illegibility = False, orientation ="", word_tags = "") for idx, bbox in enumerate(bboxes)}
+        ufo_result['images'][image_fname]['words'] = dict(words_info)
         pred_sample_bboxes[image_fname] = bboxes
         
 
@@ -94,7 +94,7 @@ def main(args):
     print('Inference in progress')
 
     ufo_result = dict(images=dict())
-    result, pred_bboxes_dict  = do_inference(model, ckpt_fpath, args.data_dir, args.input_size,
+    result, pred_bboxes_dict  = do_inference(model, ckpt_fpath, args.data_dir, args.json_dir, args.input_size,
                                     args.batch_size)
     ufo_result['images'].update(result['images'])
 
@@ -106,13 +106,13 @@ def main(args):
      
     resDict = calc_deteval_metrics(pred_bboxes_dict,  gt_bboxes_dict, transcriptions_dict=transcription,
                          eval_hparams=None, bbox_format='rect', verbose=False)
-
+    resDict = resDict['total']
     
     output_txtname = 'result.txt'
     with open(osp.join(args.output_dir, output_txtname), 'w') as f:
         f.write("#Result\n #Data: {}\n #model_pth: {}\n".format(args.data_dir, ckpt_fpath))
         f.write('DetEval/Precision: {:.6f}, DetEval/Recall: {:.6f}, DetEval/F1: {:.6f}\n'.format(
-            resDict['total']['precision'], resDict['total']['recall'], resDict['total']['hmean']))
+            resDict['precision'], resDict['recall'], resDict['hmean']))
     
 
 if __name__ == '__main__':
