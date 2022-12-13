5d81f1a081a6b80a24439af6f2272edbe236457f
KH <ganghe74@gmail.com>
2022-12-13 06:10:27 +0000

    fix: hotfix DetEval bug
    아래 에러에 대한 임시조치
    
    ```
    Model Eval/Epoch 1:
    Val/Mean loss: 4.1402, Val/Cls loss: 0.9165, Val/Angle loss: 1.3407, Val/IoU loss: 1.8830
    Traceback (most recent call last):
      File "train.py", line 282, in <module>
        main(args)
      File "train.py", line 277, in main
        do_training(**args.__dict__)
      File "train.py", line 239, in do_training
        resDict = calc_deteval_metrics(pred_bboxes_dict,  gt_bboxes_dict, transcriptions_dict=transcription,
      File "/opt/ml/level2_dataannotation_cv-level2-cv-11/code/deteval.py", line 217, in calc_deteval_metrics
        transcription = transcriptionsList[n]
    IndexError: list index out of range
    ```

--- code/deteval.py
+++ code/deteval.py
@@ -214,7 +214,10 @@ def calc_deteval_metrics(pred_bboxes_dict, gt_bboxes_dict, transcriptions_dict=N
 
         for n in range(len(pointsList)):
             points = pointsList[n]
-            transcription = transcriptionsList[n]
+            try:
+                transcription = transcriptionsList[n]
+            except:
+                transcription = ""
             dontCare = transcription == "###"
             gtRect = Rectangle(*points)
             gtRects.append(gtRect)
